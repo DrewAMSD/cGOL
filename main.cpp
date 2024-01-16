c89255737c3514bcc69085af8ef6697b622d79c0
Drew Miller <drew.miller@mrjaffesclass.com>
2024-01-16 14:26:19 -0800

    take in coords to update started

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 class Cell {
 public:
+    //0 = dead, 1 = alive
     bool state = 0;
 
     void changeCellState() {
@@ -15,6 +17,7 @@ public:
     int size;
     std::vector<std::vector<Cell> > grid;
 
+    //init grid with dead cells
     void initGrid(int n) {
         this->size = n;
         for (int i = 0; i < n; i++) {
@@ -27,28 +30,35 @@ public:
     }
 
     void printDashes() {
-        for (int i = 0; i < 5; i++) {
+        std::cout << "-";
+        for (int i = 0; i < 4; i++) {
             std::cout << " ";
         }
-        for (int i = 0; i < size*4; i++){
+        for (int i = 1; i < size*4; i++){
             std::cout << "-";
         }
         std::cout << std::endl;
     }
 
     void printBoard() {
-        for (int i = 0; i < 6; i++) {
+        //print columns indices
+        std::cout << "G";
+        for (int i = 0; i < 4; i++) {
             std::cout << " ";
         }
-        for (int i = 0; i < this->size; i++) {
-            std::cout << i << "   ";
+        for (int i = 1; i <= this->size; i++) {
+            std::cout << "-"<< i << "-";
+            if (i < 10) {
+                std::cout << " ";
+            }
         }
         std::cout << std::endl;
-        for (int i = 0; i < this->size; i++) {
+        //print rest of board
+        for (int i = 1; i <= this->size; i++) {
             printDashes();
             std::cout << i << (i < 10 ? "  " : " ");
             for (int j = 0; j < this->size; j++) {
-                std::cout << " | " << grid[i][j].state;
+                std::cout << " | " << grid[i-1][j-1].state;
             }
             std::cout << " |" << std::endl;
         }
@@ -56,10 +66,40 @@ public:
     }
 };
 
+void runGame() {
+    //runGame
+}
+
 int main() {
+    //create and init grid
     int n = 15;
     Grid grid;
     grid.initGrid(n);
     grid.printBoard();
+
+    //take in coordinates to set to alive before starting
+    std::cout << "--enter coords('row col') of cell to change its state--" << std::endl;
+    std::cout << "--input -1 -1 to start simulation--" << std::endl;
+    bool setCoords = true;
+    while (setCoords) {
+        int n, m;
+        try {
+            std::cin >> n >> m;
+            //check if start of simulation
+            if (n == -1 && m == -1) {
+                setCoords = false;
+                break;
+            }
+
+        }
+        catch (int coord) {
+            std::cout << "Error, incorrect coordinate: " << coord << std::endl;
+        }
+    }
+    std::cout << "start game";
+
+    //run simulation after setting up board
+    runGame();
+
     return 0;
 }
